4ce18d99e09934bd462a3592cd1bd1b99f6af465
werdl <116349156+werdl@users.noreply.github.com>
2023-04-24 08:18:23 +0100

    Update 99.js

--- 99.js
+++ 99.js
@@ -1,2 +1,2 @@
-z=(a)=>{return a==1?"bottle":"bottles"}
+z=(a)=>{return"bottle"+a==1?"s":''}
 b="of beer";x=99;while(x-->1){c=z(x);console.log(x,c,b,x,c,b,"take one down pass it around",x-1,z(x-1),b,"on the wall")}
